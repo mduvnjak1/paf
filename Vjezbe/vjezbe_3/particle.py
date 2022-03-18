122734e2ddb7ac369ad264d65b0ddac421bf3a13
mduvnjak1 <mduvnjak@pmfst.hr>
2022-03-18 12:37:36 +0100

    Upload

--- Vjezbe/vjezbe_3/particle.py
+++ Vjezbe/vjezbe_3/particle.py
@@ -1,23 +1,52 @@
 import numpy as np
 class particle:
     def __init__(self):
-        self
-    def set_initial_conditions(self, v_0, angle, x_0):
-        self.v_0=v_0
-        self.angle=angle
-        self.x_0=x_0
-        v
-        lista_brzina[v_0]
-        lista_kuteva[angle]
-        lista_polozaja[x_0]
+        self.lista_vremena=[]
+        self.lista_akceleracija_x=[]
+        self.lista_akceleracija_y=[]
+        self.lista_brzina_x=[]
+        self.lista_brzina_y=[]
+        self.lista_položaja_x=[]
+        self.lista_položaja_y=[]
+    def set_initial_conditions(self, v, angle, x, y):
+        vx=v*np.cos(angle)
+        vy=v*np.sin(angle)
+        self.lista_vremena.append(0)
+        self.lista_akceleracija_x.append(0)
+        self.lista_akceleracija_y.append(-9.81)
+        self.lista_brzina_x.append(vx)
+        self.lista_brzina_y.append(vy)
+        self.lista_položaja_x.append(x)
+        self.lista_položaja_y.append(y)
     def reset(self):
-        self.set_initial_conditions(0,0,0)
-    def __move():
+        self.__init__()
+    def __move(self):
+        t=self.lista_vremena[0]
+        ax=self.lista_akceleracija_x[0]
+        ay=self.lista_akceleracija_y[0]
+        vx=self.lista_brzina_x[0]
+        vy=self.lista_brzina_y[0]
+        x=self.lista_položaja_x[0]
+        y=self.lista_položaja_y[0]
         dt=0.01
-        lista_brzina[-1]
-
-
-
+        t+=dt
+        ax+=0
+        ay+=0
+        vx+=ax*dt
+        vy+=ay*dt
+        x+=vx*dt
+        y+=vy*dt
+        self.lista_vremena.append(t)
+        self.lista_akceleracija_x.append(ax)
+        self.lista_akceleracija_y.append(ay)
+        self.lista_brzina_x.append(vx)
+        self.lista_brzina_y.append(vy)
+        self.lista_položaja_x.append(x)
+        self.lista_položaja_y.append(y)
+    def range(self):
+        while self.lista_položaja_y[-1]>=0:
+            self.__move()
+        print (self.lista_položaja_x[-1])
 p1=particle()
-p2=particle(4,5,6)
-p1.
+p1.set_initial_conditions(10,1,0,0)
+p1.range()
