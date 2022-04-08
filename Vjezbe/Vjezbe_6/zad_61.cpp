fa4fe81d8f4d9bc3220f21204d825b60db36d98d
mduvnjak1 <mduvnjak@pmfst.hr>
2022-04-08 12:41:15 +0200

    Upload

---
+++ Vjezbe/Vjezbe_6/zad_61.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void pravac(float x1,float y1,float x2,float y2)
+{
+float k;
+k=(y2-y1)/(x2-x1);
+float l;
+l=y1-k*x1;
+std::cout << "y = " << k << "x + " << l << std::endl;
+}
+int main()
+{
+pravac(2,4,3,5);
+return 0;
+}
