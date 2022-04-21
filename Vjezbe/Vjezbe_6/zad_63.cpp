10a14b4785828b711ac6b63df7b9a6b3ccf09fda
mduvnjak1 <mduvnjak@pmfst.hr>
2022-04-21 17:59:04 +0200

    Upload

--- Vjezbe/Vjezbe_6/zad_63.cpp
+++ Vjezbe/Vjezbe_6/zad_63.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+
 void interval(int polje[],int size, int a, int b)
 {
-for i in range (size)
+for (int i=0; i < size; i++)
 {
-if (a < polje[i] and polje[i] < b)
+    if (a <= polje[i] and polje[i] <= b)
 {
-std::cout << polje[i] << std::endl;
+        std::cout << polje[i] << std::endl;
 }
+}
+}
+void reverse(int polje[],int size)
+{
+int rpolje[]={};
+for (int i=1; i <= size; i++)
+{
+    rpolje.push_back(polje[size-i]);
+}
+for (int i=0; i < size; i++)
+{
+    polje[i]=rpolje[i];
 }
 }
 int main()
 {
 int polje[7]={1,2,3,4,5,6,7}; 
 interval(polje,7,2,5);
+reverse(polje,7);
 return 0;
 }
