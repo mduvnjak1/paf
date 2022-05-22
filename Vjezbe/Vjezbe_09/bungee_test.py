89d2f82b5326c5e62b0941175d10e7ca8542b0f9
mduvnjak1 <mduvnjak@pmfst.hr>
2022-05-22 22:27:02 +0200

    Upload

--- Vjezbe/Vjezbe_09/bungee_test.py
+++ Vjezbe/Vjezbe_09/bungee_test.py
@@ -3,16 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 def F1(p,Cd,A,k,x0,l,v,x):
     if np.abs(x-x0)>l:
-            xe=x-l
+            xe=x-x0+l
     else:
             xe=0
     return(-0.5*p*Cd*A*v**2*np.sign(v)-k*xe)
 b1=bng.Projectile()
-b1.set_initial_conditions(0, 100, 0.3, 10, 0.5, 80, 100, 50, F1)
-b1.plot_y_t(100,0.01)
+b1.set_initial_conditions(0, 100, 0, 10, 0.5, 80, 100, 50, F1)
+b1.plot_y_t(50,0.01)
 b1.reset()
-b1.set_initial_conditions(0, 100, 0.3, 10, 0.5, 80, 100, 50, F1)
-b1.plot_y_t_rk(100,0.01)
+b1.set_initial_conditions(0, 100, 0, 10, 0.5, 80, 100, 50, F1)
+b1.plot_y_t_rk(50,0.01)
 plt.show()
 b1.reset()
 #S otporom zraka
