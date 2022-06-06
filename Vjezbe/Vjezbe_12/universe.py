87f98d6cc7ee2a270dea4c13e6b5cb101d0b99fe
mduvnjak1 <mduvnjak@pmfst.hr>
2022-06-06 17:02:13 +0200

    Upload

--- Vjezbe/Vjezbe_12/universe.py
+++ Vjezbe/Vjezbe_12/universe.py
@@ -1,6 +1,7 @@
 from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, writers
 class Planet:
     def __init__(self,name):
         self.name=name
@@ -32,11 +33,12 @@ class Planet:
         self.__init__()
     
 
-def __interact(g1,g2,F,dt=10**5):
-    if g1!=g2:
-        g1.a=F(g1,g2)/g1.m
-        #print(F(g1,g2))
-        #print(g1.a)
+def __interact(lista_objekata,F,dt=10**5):
+    for g1 in lista_objekata:
+        g1.a=np.array((0,0,0))
+        for g2 in lista_objekata:
+            if g1!=g2:
+                g1.a=g1.a+F(g1,g2)/g1.m
         g1.lista_vremena.append(g1.lista_vremena[-1]+dt)
         g1.v=g1.v+g1.a*dt
         g1.lista_v.append(g1.v)
@@ -45,57 +47,22 @@ def __interact(g1,g2,F,dt=10**5):
         g1.x.append(g1.xyz[0])
         g1.y.append(g1.xyz[1])
         g1.z.append(g1.xyz[2])
-        #print(g1.name+" "+g2.name)
-    
-def __interact_rk(g1,g2,F,dt=10**5):
-    if g1!=g2:
-        g1k1v=(F(g1,g2)/g1.m)*dt
-        g1k1=g1.v*dt
-        g1k2v=(F(g1,g2)/g1.m)*dt
-        g1k2=(g1.v+0.5*g1k1v)*dt
-        g1k3v=(F(g1,g2)/g1.m)*dt
-        g1k3=(g1.v+0.5*g1k2v)*dt
-        g1k4v=(F(g1,g2)/g1.m)*dt
-        g1k4=(g1.v+g1k3v)*dt
-        g1.lista_vremena.append(g1.lista_vremena[-1]+dt)
-        g1.v=g1.v+(g1k1v+2*g1k2v+2*g1k3v+g1k4v)/6
-        g1.lista_v.append(g1.v)
-        g1.xyz=g1.xyz+(g1k1+2*g1k2+2*g1k3+g1k4)/6
-        g1.lista_xyz.append(g1.xyz)
-        g1.x.append(g1.xyz[0])
-        g1.y.append(g1.xyz[1])
-        g1.z.append(g1.xyz[2])
-        g1.a=F(g1,g2)/g1.m
-        g1.lista_a.append(g1.a)
-    
-def plot_trajectory(g1,g2,F,dt=10**5,T=31.557*10**6): 
-    while (g1.lista_vremena[-1]<=T and g2.lista_vremena[-1]<=T):
-        __interact(g1,g2,F,dt)
-    plt.plot(g1.x,g1.y,label="Zemlja")
-    plt.plot(g2.x,g2.y,label="Sunce")
-    plt.legend(loc="upper left")
 
-def plot_trajectory_rk(g1,g2,F,dt=10**5,T=31.557*10**6):  
-    while (g1.lista_vremena[-1]<=T and g2.lista_vremena[-1]<=T):
-        __interact_rk(g1,g2,F,dt)
-    plt.plot(g1.x,g1.y,label="Zemlja_rk")
-    plt.plot(g2.x,g2.y,label="Sunce_rk")
-    plt.legend(loc="upper left")
-
-def universe_plot_trajectory(lista_objekata,F,dt=10**5,T=31.557*10**7):   
+def universe_plot_trajectory(lista_objekata,F,dt=10**5,T=5*31.557*10**6):   
     while (lista_objekata[0].lista_vremena[-1]<=T):
-        for g1 in lista_objekata:
-            for g2 in lista_objekata:
-                __interact(g1,g2,F,dt)
+        __interact(lista_objekata,F,dt)
     for element in lista_objekata:
         plt.plot(element.x,element.y,label=str(element.name))
     plt.legend(loc="upper left")
 
-def universe_plot_trajectory_rk(lista_objekata,F,dt=10**5,T=31.557*10**7):   
+def universe_animate_trajectory(lista_objekata,F,dt=5*10**5,T=5*31.557*10**6):   
     while (lista_objekata[0].lista_vremena[-1]<=T):
-        for g1 in lista_objekata:
-            for g2 in lista_objekata:
-                __interact_rk(g1,g2,F,dt)
-    for element in lista_objekata:
-        plt.plot(element.x,element.y,label=str(element.name)+" rk")
-    plt.legend(loc="upper left")
+        __interact(lista_objekata,F,dt)
+        plt.xlim(-2.5*10**11,2.5*10**11)
+        plt.ylim(-2.5*10**11,2.5*10**11)
+        for element in lista_objekata:
+            plt.scatter(element.xyz[0],element.xyz[1],label=str(element.name))
+            plt.plot(element.x,element.y)
+            plt.legend(loc="upper left")
+        plt.pause(0.0001)
+        plt.clf()
--- Vjezbe/Vjezbe_12/universe_test.py
+++ Vjezbe/Vjezbe_12/universe_test.py
@@ -3,25 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 def F1(g1,g2,G=6.67408*10**(-11)):
     return(G*g1.m*g2.m*(g2.xyz-g1.xyz)/(np.linalg.norm(g2.xyz-g1.xyz))**3)
+
 Sun=U.Planet("Sun")
 Mercury=U.Planet("Mercury")
 Venus=U.Planet("Venus")
 Earth=U.Planet("Earth")
 Mars=U.Planet("Mars")
-Jupyter=U.Planet("Jupyter")
-Saturn=U.Planet("Saturn")
-Uranus=U.Planet("Uranus")
-Neptune=U.Planet("Neptune")
-Sun.set_initial_conditions(np.array((0,0,0)),np.array((0,0,0)),1.989*10**30)
 
+Sun.set_initial_conditions(np.array((0,0,0)),np.array((0,0,0)),1.989*10**30)
 Mercury.set_initial_conditions(np.array((57.9*10**9,0,0)),np.array((0,47.4*10**(3),0)),0.33*10**24)
 Venus.set_initial_conditions(np.array((108.2*10**9,0,0)),np.array((0,35*10**(3),0)),4.87*10**24)
 Earth.set_initial_conditions(np.array((149.6*10**9,0,0)),np.array((0,29.783*10**(3),0)),5.9742*10**24)
 Mars.set_initial_conditions(np.array((228*10**9,0,0)),np.array((0,24.1*10**(3),0)),0.642*10**24)
-Jupyter.set_initial_conditions(np.array((778.5*10**9,0,0)),np.array((0,13.1*10**(3),0)),1898*10**24)
-Saturn.set_initial_conditions(np.array((1432*10**9,0,0)),np.array((0,9.7*10**(3),0)),568*10**24)
-Uranus.set_initial_conditions(np.array((2867*10**9,0,0)),np.array((0,6.8*10**(3),0)),86.8*10**24)
-Neptune.set_initial_conditions(np.array((4515*10**9,0,0)),np.array((0,5.4*10**(3),0)),102*10**24)
-lista_objekata=[Sun, Mercury, Venus, Earth, Mars, Jupyter, Saturn, Uranus, Neptune]
+lista_objekata=[Sun, Mercury, Venus, Earth, Mars]
 U.universe_plot_trajectory(lista_objekata,F1)
 plt.show()
+
+Sun=U.Planet("Sun")
+Mercury=U.Planet("Mercury")
+Venus=U.Planet("Venus")
+Earth=U.Planet("Earth")
+Mars=U.Planet("Mars")
+
+Sun.set_initial_conditions(np.array((0,0,0)),np.array((0,0,0)),1.989*10**30)
+Mercury.set_initial_conditions(np.array((57.9*10**9,0,0)),np.array((0,47.4*10**(3),0)),0.33*10**24)
+Venus.set_initial_conditions(np.array((108.2*10**9,0,0)),np.array((0,35*10**(3),0)),4.87*10**24)
+Earth.set_initial_conditions(np.array((149.6*10**9,0,0)),np.array((0,29.783*10**(3),0)),5.9742*10**24)
+Mars.set_initial_conditions(np.array((228*10**9,0,0)),np.array((0,24.1*10**(3),0)),0.642*10**24)
+lista_objekata=[Sun, Mercury, Venus, Earth, Mars]
+U.universe_animate_trajectory(lista_objekata,F1)
