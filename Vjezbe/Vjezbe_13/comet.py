91b7c7a9f80777e3f7779da243d49a97bfbee1ad
mduvnjak1 <mduvnjak@pmfst.hr>
2022-06-14 11:53:35 +0200

    Upload

--- Vjezbe/Vjezbe_13/comet.py
+++ Vjezbe/Vjezbe_13/comet.py
@@ -47,9 +47,8 @@ def __interact(lista_objekata,F,dt=10**4):
                 rp=g1.r
                 g1.reset(g1.name + " " + g2.name)
                 g1.set_initial_conditions((xyzp*mp+g2.xyz*g2.m)/(mp+g2.m), (vp*mp+g2.v*g2.m)/(mp+g2.m), (mp+g2.m), (rp*mp+g2.r*g2.m)/(mp+g2.m))
-                print(g1.v)
                 lista_objekata.remove(g2)
-            if g1!=g2:
+            if (g1!=g2) and (np.linalg.norm(g2.xyz-g1.xyz)>=(g1.r+g2.r)):
                 g1.a=g1.a+F(g1,g2)/g1.m
         g1.lista_vremena.append(g1.lista_vremena[-1]+dt)
         g1.v=g1.v+g1.a*dt
@@ -67,7 +66,7 @@ def universe_plot_trajectory(lista_objekata,F,dt=10**4,T=5*31.557*10**6):
         plt.plot(element.x,element.y,label=str(element.name))
     plt.legend(loc="upper left")
 
-def universe_animate_trajectory(lista_objekata,F,dt=5*10**4,T=5*31.557*10**6):   
+def universe_animate_trajectory(lista_objekata,F,dt=10**4,T=5*31.557*10**6):   
     while (lista_objekata[0].lista_vremena[-1]<=T):
         __interact(lista_objekata,F,dt)
         plt.xlim(-3.5*10**11,3.5*10**11)
@@ -85,16 +84,11 @@ def __interact_hit(lista_objekata,F,dt=10**4):
         for g2 in lista_objekata:
             if (g1!=g2) and (np.linalg.norm(g2.xyz-g1.xyz)<=(g1.r+g2.r)):
                 print("hit"+" "+g1.name+" "+g2.name)
-            if g1!=g2:
+            if (g1!=g2) and (np.linalg.norm(g2.xyz-g1.xyz)>=(g1.r+g2.r)):
                 g1.a=g1.a+F(g1,g2)/g1.m
         g1.lista_vremena.append(g1.lista_vremena[-1]+dt)
         g1.v=g1.v+g1.a*dt
-        g1.lista_v.append(g1.v)
         g1.xyz=g1.xyz+g1.v*dt
-        g1.lista_xyz.append(g1.xyz)
-        g1.x.append(g1.xyz[0])
-        g1.y.append(g1.xyz[1])
-        g1.z.append(g1.xyz[2])
 
 def universe_test_hit(lista_objekata,F,dt=10**4,T=1*31.557*10**6):   
     while (lista_objekata[0].lista_vremena[-1]<=T):
